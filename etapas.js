d857f18c19af745a442aa96ac83d8642aa912f01
Carlos F. Laranjeira Jr <junior.laranjeira@hotmail.com>
2021-07-30 14:49:35 -0300

    Preparando ambiente para os candidatos

---
+++ etapas.js
@@ -0,0 +1,49 @@
+let etapas = [
+    {
+        titulo: 'SENADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    {url: 'candidato.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    {url: 'candidato.jpg',legenda: 'Vereador',small: false}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    {url: 'candidato.jpg', legenda: 'Presidente', small: false},
+                    {url: 'candidato.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: 84,
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    {url: 'candidato.jpg', legenda: 'Presidente', small: false},
+                    {url: 'candidato.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            }
+        ]
+    }
---
+++ script.js
@@ -0,0 +1,24 @@
+/*Variáveis de controle de interface*/ 
+let seuVotoPara = document.querySelector('.d-1-1 span');
+let cargo = document.querySelector('.d-1-2 span');
+let descricao = document.querySelector('.d-1-4 ');
+let aviso = document.querySelector('.d-2');
+let lateral = document.querySelector('.d-1-right'); //fotos dos candidatos
+let numeros = document.querySelector('.d-1-3'); //numeros dos candidatos
+
+/*Variáveis de ambiente*/ 
+let etapaAtual = 0; 
+
+function clicou(n) {
+    
+}
+
+function branco() {
+    
+}
+
+function corrige() {
+}    
+
+function confirma() {
+}
